cd88883f6a7dedf4db1cadb418e9c899451516b4
Aswinpkrishnan94 <78928945+Aswinpkrishnan94@users.noreply.github.com>
2021-10-18 18:58:17 +0100

    Day 1 Project - Band Name Generator
    The program asks for the city that you grew up and the name of your pet.
    It is then generate the name by combining both the strings.
    method used: String concatenation

---
+++ Python/Day 1/Band_name_generator.py
@@ -0,0 +1,6 @@
+
+print("Welcome to the Band Name Generator")
+city=input("What city did you grow up in\n")
+name=input("What is the name of your pet\n")
+band_name=city+" "+name
+print("The name of your band is "+band_name)
